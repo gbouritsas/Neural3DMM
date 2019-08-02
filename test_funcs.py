e495ecf650047847b1fbbfacce9fb1449a291933
Georgios Bouritsas <gb318@rosella.doc.ic.ac.uk>
2019-08-03 00:41:00 +0100

    updated notebook and test_funcs

--- test_funcs.py
+++ test_funcs.py
@@ -3,10 +3,8 @@ import copy
 from tqdm import tqdm
 import numpy as np
 
-
 def test_autoencoder_dataloader(device, model, dataloader_test, shapedata, mm_constant = 1000):
     model.eval()
-    loss = 0
     l1_loss = 0
     l2_loss = 0
     shapedata_mean = torch.Tensor(shapedata.mean).to(device)
@@ -20,13 +18,17 @@ def test_autoencoder_dataloader(device, model, dataloader_test, shapedata, mm_co
             else:
                 predictions = torch.cat([predictions,prediction],0) 
                 
-            x_recon = prediction[:,:-1]
-            x = tx[:,:-1]
+            if dataloader_test.dataset.dummy_node:
+                x_recon = prediction[:,:-1]
+                x = tx[:,:-1]
+            else:
+                x_recon = prediction
+                x = tx
             l1_loss+= torch.mean(torch.abs(x_recon-x))*x.shape[0]/float(len(dataloader_test.dataset))
             
             x_recon = (x_recon * shapedata_std + shapedata_mean) * mm_constant
             x = (x * shapedata_std + shapedata_mean) * mm_constant
-            l2_loss+= torch.mean(torch.sqrt(torch.sum((x_recon - x)**2,dim = 2)))*x.shape[0]/float(len(dataloader_test.dataset))
+            l2_loss+= torch.mean(torch.sqrt(torch.sum((x_recon - x)**2,dim=2)))*x.shape[0]/float(len(dataloader_test.dataset))
             
         predictions = predictions.cpu().numpy()
         l1_loss = l1_loss.item()
